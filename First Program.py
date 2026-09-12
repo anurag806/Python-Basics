04fcdd0a39795250d142a557c747f19fec39ea39
anurag pandey <anuragpandey930565@gmail.com>
2026-09-12 06:25:01 +0530

    Basic learnings

--- First Program.py
+++ First Program.py
@@ -1,4 +1,4 @@
-print("I love python")
+'''print("I love python")
 a=6;
 b="anurag pandey"
 is_true=True;
@@ -7,3 +7,31 @@ print(type(a),a)
 print(type(b),b)
 print(type(is_true),is_true)
 print(type(e),e)
+name=str(input("enter ur name"));
+age=int(input("enter ur age"));
+print(type(name),name)
+print(type(age),age)
+salary=float(input("enter ur salary"))
+print(salary)
+'''
+'''name=str(input("enter ur name"))
+age=int(input("enter ur age"))
+salary=float(input("enter ur salary"))
+print(name,age,salary);
+anual_salary=salary*12;
+print(anual_salary);
+'''
+#arithmetic operations
+a=10;
+b=20;
+print(a+b,a-b,a*b,a/b)
+print(a**b,a%b,a//b)
+#comparsion operations
+print(a>=b,a<=b,a!=b,a==b)
+#logical operation
+age = 28
+salary = 40000
+
+print(age > 18 and salary > 50000)
+print(age > 18 or salary > 50000)
+print(not age > 18)
