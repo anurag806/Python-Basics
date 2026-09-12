b1f6d388c021e871014a189df67361bedd96ebfd
anurag pandey <anuragpandey930565@gmail.com>
2026-09-12 06:50:27 +0530

    Loops

---
+++ Second.py
@@ -0,0 +1,22 @@
+marks=int(input("Enter your marks:"))
+if marks>=90:
+    print("Grade A")
+elif marks>=75 and marks<=89:
+    print("Grade B")
+elif marks>=50 and marks<=74:
+    print("Grade C")
+else:
+    print("Fail")
+#loops
+for i in range(0,5):
+    print("i am anurag")
+for i in range(0,20,5):
+    print(i);
+for j in range(10,0,-1):
+    print(j)
+
+#while loops
+i=10;
+while i <=20:
+    print(i);
+    i+=1;
