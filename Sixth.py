fa5c806bafe872dec47761a06c7fcc31749a3207
anurag pandey <anuragpandey930565@gmail.com>
2026-09-12 20:18:39 +0530

    file handling

---
+++ Sixth.py
@@ -0,0 +1,18 @@
+# file handlings
+file=open("employees.txt","w")
+file.write("anurag,pandey,5000")
+#file.close();
+file=open("employees.txt","r")
+data=file.read();
+print(data);
+file=open("sam.txt","w");
+file.write("Anurag,IT,63000\nRahul,HR,45000\nArya,Finance,72000")
+file=open("sam.txt","r")
+data=file.read();
+print(data);
+file=open("Sam.txt","a");
+file.write("\nAmit,IT,86000")
+file.close();
+file=open("Sam.txt","r")
+datas=file.read();
+print(datas);
